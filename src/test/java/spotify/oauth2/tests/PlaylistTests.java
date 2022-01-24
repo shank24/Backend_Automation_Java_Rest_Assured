d327bf9d37552d7d5c536038933b54cf0c17d12b
shankkalra <shankkalra@gmail.com>
2022-01-24 23:28:12 +0530

    Set Up class

--- src/test/java/spotify/oauth2/tests/PlaylistTests.java
+++ src/test/java/spotify/oauth2/tests/PlaylistTests.java
@@ -1,6 +1,13 @@
 package spotify.oauth2.tests;
 
+import io.restassured.builder.RequestSpecBuilder;
+import org.testng.annotations.BeforeClass;
+
 public class PlaylistTests {
 
+    @BeforeClass
+    public void setup(){
+
+    }
 
 }
