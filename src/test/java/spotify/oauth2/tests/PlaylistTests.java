99d23f859f4cc733f5bbd44132758c577e61f0ef
shankkalra <shankkalra@gmail.com>
2022-01-27 15:47:51 +0530

    Rest Spec

--- src/test/java/spotify/oauth2/tests/PlaylistTests.java
+++ src/test/java/spotify/oauth2/tests/PlaylistTests.java
@@ -1,13 +1,45 @@
 package spotify.oauth2.tests;
 
+import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.testng.annotations.BeforeClass;
 
 public class PlaylistTests {
 
+    RequestSpecification requestSpecification;
+    ResponseSpecification responseSpecification;
+
     @BeforeClass
     public void setup(){
 
+
+        /**
+         * RequestSpec Builder
+         */
+        RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder()
+                .setBaseUri("https://accounts.spotify.com")
+                .setBasePath("/v1")
+                .addHeader("Authorization","Bearer ")
+        //requestSpecBuilder.addHeader("x-api-key", ObjectReader.reader.getKey());
+                .setContentType(ContentType.JSON)
+                .log(LogDetail.ALL);
+
+        requestSpecification = requestSpecBuilder.build();
+
+        /**
+         * ResponseSpec Builder
+         */
+        ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder()
+                //.expectStatusCode(201)
+                .expectContentType(ContentType.JSON)
+                .log(LogDetail.ALL);
+
+        responseSpecification = responseSpecBuilder.build();
     }
 
 }
