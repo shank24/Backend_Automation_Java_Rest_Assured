2c385f4c7c64990571b706dd55dcc7c82a0e8b3b
shankkalra <shankkalra@gmail.com>
2022-02-01 19:04:29 +0530

    Token Manager

--- src/test/java/com/spotify/oauth2/api/TokenManager.java
+++ src/test/java/com/spotify/oauth2/api/TokenManager.java
@@ -1,5 +1,37 @@
 package com.spotify.oauth2.api;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import java.util.HashMap;
+import static com.spotify.oauth2.api.SpecBuilder.getResponseSpec;
+import static io.restassured.RestAssured.given;
+
 public class TokenManager {
 
+    public static String renewToken(){
+
+        HashMap<String,String> formParams = new HashMap<>();
+        formParams.put("refresh_token","AQAqEshO1DXJXKUuHpvO_mC4xj6-hdt9giUhNoEixy5Npp3D3EfaDgxLiAgJ6zAVjnCJZy0GBmnRVtGXoopko0nfbeSzw7pCtx54aeUKF3ZsZuv4XbwDNs37XHtKrDaUg6Y");
+        formParams.put("grant_type","refresh_token");
+        formParams.put("client_id","d20b8dc9abd34ee38d3eaee105b7c88c");
+        formParams.put("client_secret","f8a7d0b2f06e45a5affe6d96ac41feb3");
+
+        Response response = given()
+                .baseUri("https://accounts.spotify.com")
+                .contentType(ContentType.URLENC)
+                .formParams(formParams)
+                .when()
+                .post("/api/token")
+                .then()
+                .spec(getResponseSpec())
+                .extract()
+                .response();
+
+        if(response.statusCode()!=200) {
+            throw new RuntimeException("ABORT !!!! Renew Token Failed");
+        }
+        return response.path("access_token");
+
+    }
 }
--- src/test/java/com/spotify/oauth2/api/applicationApi/PlaylistApi.java
+++ src/test/java/com/spotify/oauth2/api/applicationApi/PlaylistApi.java
@@ -4,13 +4,15 @@ import com.spotify.oauth2.api.RestResource;
 import com.spotify.oauth2.pojo.Playlist;
 import io.restassured.response.Response;
 
+import static com.spotify.oauth2.api.TokenManager.renewToken;
+
 public class PlaylistApi {
 
     static String userId = "31uxnqcuy7hrnntpso5yaoe56uzy";
-    static String access_Token = "BQCPJRbaksYUgOcVL4TcQdO3Uw5gukdaG_W30SujnnahjLGMf9rwm9P36i8NZbURxufHSM0kMUZYr5c7Zi3-MxB8Owzy1BrZDLKNsZCPjywmAzAcl9_rjRWoFWQ1MzTfAt2huInXsRuDHU4d5sY4iw-KPWnmv6GkmSyY2_bAtcomIfWzvR0FqO8wwZUSRXyyW5QBw58OhemmKZZIKdQ1OSqLkYKanvlPohNRwaXmYFyL5GGq";
+    //static String access_Token = "BQCPJRbaksYUgOcVL4TcQdO3Uw5gukdaG_W30SujnnahjLGMf9rwm9P36i8NZbURxufHSM0kMUZYr5c7Zi3-MxB8Owzy1BrZDLKNsZCPjywmAzAcl9_rjRWoFWQ1MzTfAt2huInXsRuDHU4d5sY4iw-KPWnmv6GkmSyY2_bAtcomIfWzvR0FqO8wwZUSRXyyW5QBw58OhemmKZZIKdQ1OSqLkYKanvlPohNRwaXmYFyL5GGq";
 
     public static Response post(Playlist requestPlaylist) {
-        return RestResource.post(requestPlaylist,access_Token,"/users/" + userId + "/playlists");
+        return RestResource.post(requestPlaylist,renewToken(),"/users/" + userId + "/playlists");
     }
 
     public static Response post(Playlist requestPlaylist, String token) {
@@ -18,11 +20,11 @@ public class PlaylistApi {
     }
 
     public static Response get(String playlistId){
-        return RestResource.get(access_Token, "/playlists/"+playlistId);
+        return RestResource.get(renewToken(), "/playlists/"+playlistId);
     }
 
     public static Response put(Playlist requestPlaylist, String playlistId)
     {
-        return RestResource.put(requestPlaylist,access_Token,"/playlists/" + playlistId);
+        return RestResource.put(requestPlaylist,renewToken(),"/playlists/" + playlistId);
     }
 }
