eede8e5678ce869b2ed79c2c148e19fea744f745
shankkalra <shankkalra@gmail.com>
2022-02-03 20:55:49 +0530

    Optimize Imports

--- src/test/java/com/spotify/oauth2/api/RestResource.java
+++ src/test/java/com/spotify/oauth2/api/RestResource.java
@@ -1,16 +1,10 @@
 package com.spotify.oauth2.api;
 
-import com.spotify.oauth2.pojo.Playlist;
-import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-
 import java.util.HashMap;
-
 import static com.spotify.oauth2.api.Route.API;
 import static com.spotify.oauth2.api.Route.TOKEN;
-import static com.spotify.oauth2.api.SpecBuilder.getAccountRequestSpec;
-import static com.spotify.oauth2.api.SpecBuilder.getRequestSpec;
-import static com.spotify.oauth2.api.SpecBuilder.getResponseSpec;
+import static com.spotify.oauth2.api.SpecBuilder.*;
 import static io.restassured.RestAssured.given;
 
 public class RestResource {
